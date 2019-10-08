162578dfeb5706b1722b72fbee49a2f1a5a98e4a
Paula Fernández López <35449028+PaulaFdez@users.noreply.github.com>
2019-10-08 09:33:38 +0200

    Update ejercicio2.java

--- Tema 4/ejercicio2.java
+++ Tema 4/ejercicio2.java
@@ -9,21 +9,43 @@ public class ejercicio2{
     System.out.println("Introduce la hora para que te salude correctamente :) :");
     hora = s.nextFloat();
     
+   // if ((hora >=6) && (hora <= 12)){
+   //    System.out.println("Buenos Días!");
+   // }
+    
+   // if ((hora >=13) && (hora <=20)){
+   //   System.out.println("Buenas Tardes!");
+   // } 
+    
+   // if ((hora >=21) && (hora <=24)){
+   //  System.out.println("Buenas noches!");
+   // }
+    
+   // if ((hora >=0) && (hora <=5)){
+   //  System.out.println("Buenas noches!");
+   // }
+    
     if ((hora >=6) && (hora <= 12)){
       System.out.println("Buenos Días!");
-    }
-    
-    if ((hora >=13) && (hora <=20)){
+    } else if ((hora >=13) && (hora <=20)){
       System.out.println("Buenas Tardes!");
-    } 
-    
-    if ((hora >=21) && (hora <=24)){
+    } else if ((hora >=21) && (hora <=24)){
+      System.out.println("Buenas noches!");
+    } else if ((hora >=0) && (hora <=5)){
       System.out.println("Buenas noches!");
     }
     
-    if ((hora >=0) && (hora <=5)){
-      System.out.println("Buenas noches!");
-    }
-  
+    //Esta última forma sería más correcta ya que el programa es más eficiente. 
+    //Tambíen podríamos haberlo hecho con un switch, declarando
+    //switch (num) {
+    //case 1: case 2: case 3:
+    //stuff
+    // break;
+    //case 4: case 5: case 6: 
+    //more stuff
+    //break;
+    //default:
+}
+        
   }
 }
